e23b3f8044165c04e74b07dd12d669e52369979f
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-04-22 21:00:05 -0400

    fixed users mig model and seeds from starter

---
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,19 @@
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+   <div
+   className="dashboard-container"
+   >
+
+    <h1
+    className="landing-page-title"
+    >ARCANA ACADEMY</h1>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi placeat, laboriosam libero consequatur quasi nesciunt dolorem vitae odit quas at explicabo autem reprehenderit itaque, reiciendis enim dignissimos unde similique quae?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus! Quod, cumque. Quisquam, voluptatibus! Quod, cumque. Quisquam, voluptatibus! Quod, cumque. Quisquam, voluptatibus! Quod, cumque.</p>
+   </div>
+  );
+}
+
+export default LandingPage;
