88e5e5b8ae63a16e052d8936422b44176aafb3f4
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-05-06 21:00:24 -0400

    added more seeders and stuff

--- app/seeds/players.py
+++ app/seeds/players.py
@@ -4,25 +4,25 @@ from sqlalchemy.sql import text
 
 def seed_players():
     player1 = Player(
-        user_id=1, name='MERLIN', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Wizard', element='TIME', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='MERLIN', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Wizard', element='TIME', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
     player2 = Player(
-        user_id=1, name='GANDALF', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Wizard', element='LIGHTNING', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='GANDALF', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Wizard', element='LIGHTNING', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
     player3 = Player(
-        user_id=1, name='HARRY', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Wizard', element='FIRE', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='HARRY', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Wizard', element='FIRE', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
     player4 = Player(
-        user_id=1, name='FRODO', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Rogue', element='EARTH', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='FRODO', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Rogue', element='EARTH', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
     player5 = Player(
-        user_id=1, name='ARAGORN', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Rogue', element='WIND', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='ARAGORN', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Rogue', element='WIND', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
     player6 = Player(
-        user_id=1, name='LEGOLAS', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Rogue', element='WATER', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='LEGOLAS', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Rogue', element='WATER', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
     player7 = Player(
-        user_id=1, name='THORIN', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Warrior', element='EARTH', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='THORIN', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Warrior', element='EARTH', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
     player8 = Player(
-        user_id=1, name='GIMLI', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Warrior', element='FIRE', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='GIMLI', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Warrior', element='FIRE', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
     player9 = Player(
-        user_id=1, name='THOR', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Warrior', element='LIGHTNING', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='THOR', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Warrior', element='LIGHTNING', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
     player10 = Player(
-        user_id=1, name='LUCY', url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",magic_class='Paladin', element='WATER', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
+        user_id=1, name='LUCY', url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746577886/MleCHOlqrusJ0qPu5Klb--0--augsy_bg-rmvd_qzlic8.png",magic_class='Paladin', element='WATER', level=1, xp=0, gold=100, health=100, mana=50, damage=10, speed=5, strength=10, intellect=5, dexterity=5, vitality=10)
 
 
     db.session.add(player1)
--- app/seeds/spells.py
+++ app/seeds/spells.py
@@ -4,37 +4,37 @@ from sqlalchemy.sql import text
 
 def seed_spells():
     spell1 = Spell(
-     name='Volcano', user_id= 1,element='Fire', cost=100, mana_cost=30, damage=670.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Unleash a fiery eruption that engulfs the battlefield, dealing massive damage to all enemies in range.')
+     name='Volcano', user_id= 1,element='Fire', cost=100, mana_cost=30, damage=670.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746579045/fire_cool_ya_nmjb0e.gif",description='Unleash a fiery eruption that engulfs the battlefield, dealing massive damage to all enemies in range.')
     spell2 = Spell(
-     name='Frost Nova', user_id= 1,element='Ice', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Freeze your enemies in place with a blast of icy wind, dealing damage and immobilizing them for a short duration.')
+     name='Frost Nova', user_id= 1,element='Ice', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1745476419/icebloomgif_cs2qx7.gif",description='Freeze your enemies in place with a blast of icy wind, dealing damage and immobilizing them for a short duration.')
     spell3 = Spell(
-     name='Lightning Bolt', user_id= 1,element='Lightning', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Call down a bolt of lightning to strike your enemies, dealing massive damage and stunning them for a short duration.')
+     name='Lightning Bolt', user_id= 1,element='Lightning', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746578575/lightning_ya_x4xdc0.gif",description='Call down a bolt of lightning to strike your enemies, dealing massive damage and stunning them for a short duration.')
     spell4 = Spell(
-     name='Earthquake', user_id= 1,element='Earth', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Create a massive earthquake that shakes the ground and deals damage to all enemies in range.')
+     name='Earthquake', user_id= 1,element='Earth', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746579060/time_ya_b6nfdm.gif",description='Create a massive earthquake that shakes the ground and deals damage to all enemies in range.')
     spell5 = Spell(
-     name='Thunderstorm', user_id= 1,element='Lightning', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Summon a powerful thunderstorm that strikes your enemies with lightning, dealing massive damage and stunning them for a short duration.')
+     name='Thunderstorm', user_id= 1,element='Lightning', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746578575/lightning_ya_x4xdc0.gif",description='Summon a powerful thunderstorm that strikes your enemies with lightning, dealing massive damage and stunning them for a short duration.')
     spell6 = Spell(
-     name='Shadow Bolt', user_id= 1,element='Shadow', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Launch a bolt of dark energy at your enemies, dealing damage and reducing their defenses for a short duration.')
+     name='Shadow Bolt', user_id= 1,element='Shadow', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1745476399/darkvidgif_kkahjb.gif",description='Launch a bolt of dark energy at your enemies, dealing damage and reducing their defenses for a short duration.')
     spell7 = Spell(
-     name='Holy Light', user_id= 1,element='Light', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Heal your allies with a burst of holy energy, restoring their health and removing negative effects.')
+     name='Holy Light', user_id= 1,element='Light', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746579060/time_ya_b6nfdm.gif",description='Heal your allies with a burst of holy energy, restoring their health and removing negative effects.')
     spell8 = Spell(
-     name='Water Surge', user_id= 1,element='Water', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Unleash a powerful surge of water that deals damage to all enemies in range and heals your allies.')
+     name='Water Surge', user_id= 1,element='Water', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746578784/water_ya_ojeele.gif",description='Unleash a powerful surge of water that deals damage to all enemies in range and heals your allies.')
     spell9 = Spell(
-     name='Wind Blade', user_id= 1,element='Wind', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Slice through your enemies with a blade of wind, dealing damage and knocking them back.')
+     name='Wind Blade', user_id= 1,element='Wind', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1745476399/darkvidgif_kkahjb.gif",description='Slice through your enemies with a blade of wind, dealing damage and knocking them back.')
     spell10 = Spell(
-     name='Earth Shield', user_id= 1,element='Earth', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Create a protective shield of earth around your allies, reducing damage taken and increasing their defenses.')
+     name='Earth Shield', user_id= 1,element='Earth', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1745476399/darkvidgif_kkahjb.gif",description='Create a protective shield of earth around your allies, reducing damage taken and increasing their defenses.')
     spell11 = Spell(
-     name='Fireball', user_id= 1,element='Fire', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Launch a fiery ball of energy at your enemies, dealing damage and igniting them for additional damage over time.')
+     name='Fireball', user_id= 1,element='Fire', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1745476399/darkvidgif_kkahjb.gif",description='Launch a fiery ball of energy at your enemies, dealing damage and igniting them for additional damage over time.')
     spell12 = Spell(
-     name='Ice Spike', user_id= 1,element='Ice', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Launch a spike of ice at your enemies, dealing damage and slowing their movement speed.')
+     name='Ice Spike', user_id= 1,element='Ice', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1745476419/icebloomgif_cs2qx7.gif",description='Launch a spike of ice at your enemies, dealing damage and slowing their movement speed.')
     spell13 = Spell(
-     name='Shadow Step', user_id= 1,element='Shadow', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Teleport behind your enemies and deal damage, stunning them for a short duration.')
+     name='Shadow Step', user_id= 1,element='Shadow', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1745476399/darkvidgif_kkahjb.gif",description='Teleport behind your enemies and deal damage, stunning them for a short duration.')
     spell14 = Spell(
-     name='Light Beam', user_id= 1,element='Light', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Unleash a beam of holy light that deals damage to your enemies and heals your allies.')
+     name='Light Beam', user_id= 1,element='Light', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1745476399/darkvidgif_kkahjb.gif",description='Unleash a beam of holy light that deals damage to your enemies and heals your allies.')
     spell15 = Spell(
-     name='Water Shield', user_id= 1,element='Water', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Create a protective shield of water around your allies, reducing damage taken and increasing their mana regeneration.')
+     name='Water Shield', user_id= 1,element='Water', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1746578784/water_ya_ojeele.gif",description='Create a protective shield of water around your allies, reducing damage taken and increasing their mana regeneration.')
     spell16 = Spell(
-     name='Hurricane', user_id= 2,element='Water', cost=100, mana_cost=30, damage=6070.80, url = "https://img.freepik.com/premium-psd/monster-cartoon-character-design-illustration-transparent-background_187482-2075.jpg",description='Create a protective shield of water around your allies, reducing damage taken and increasing their mana regeneration.')
+     name='Hurricane', user_id= 2,element='Water', cost=100, mana_cost=30, damage=6070.80, url = "https://res.cloudinary.com/dl6ls3rgu/image/upload/v1745476399/darkvidgif_kkahjb.gif",description='Create a protective shield of water around your allies, reducing damage taken and increasing their mana regeneration.')
 
 
 
