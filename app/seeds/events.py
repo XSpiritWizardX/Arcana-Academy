136617a5ee87e13b7c53b80f746779754101c266
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-05-07 22:11:38 -0400

    render 3

--- app/seeds/events.py
+++ app/seeds/events.py
@@ -4,34 +4,34 @@ from sqlalchemy.sql import text
 
 def seed_events():
     event1 = Event(
-         name='Wand Festival', time="8pm", date="01/03/2026", description="Festival of wands starts in January and and ends in March. This festival is a celebration of the wand and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new wands and accessories."
+         name='Wand Festival', time="8pm", date="01/03/2026", description="Festival of wands starts in January and and ends in ."
     )
     event2 = Event(
-         name='Potion Festival', time="8pm", date="01/03/2026", description="Festival of potions starts in January and and ends in March. This festival is a celebration of the potion and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new potions and accessories."
+         name='Potion Festival', time="8pm", date="01/03/2026", description="Festival of potions starts in January and and ends in Marc."
     )
     event3 = Event(
-         name='Spell Festival', time="8pm", date="01/03/2026", description="Festival of spells starts in January and and ends in March. This festival is a celebration of the spell and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new spells and accessories."
+         name='Spell Festival', time="8pm", date="01/03/2026", description="Festival of spells starts in January and and ends in Ma."
     )
     event4 = Event(
-         name='Monster Festival', time="8pm", date="01/03/2026", description="Festival of monsters starts in January and and ends in March. This festival is a celebration of the monster and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new monsters and accessories."
+         name='Monster Festival', time="8pm", date="01/03/2026", description="Festival of monsters starts in January and and ends in March.."
     )
     event5 = Event(
-         name='Wizard Festival', time="8pm", date="01/03/2026", description="Festival of wizards starts in January and and ends in March. This festival is a celebration of the wizard and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new wizards and accessories."
+         name='Wizard Festival', time="8pm", date="01/03/2026", description="Festival of wizards starts in January and and ends in Marc."
     )
     event6 = Event(
-         name='Dragon Festival', time="8pm", date="01/03/2026", description="Festival of dragons starts in January and and ends in March. This festival is a celebration of the dragon and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new dragons and accessories."
+         name='Dragon Festival', time="8pm", date="01/03/2026", description="Festival of dragons starts in January and and ends in Marc."
     )
     event7 = Event(
-         name='Element Festival', time="8pm", date="01/03/2026", description="Festival of elements starts in January and and ends in March. This festival is a celebration of the element and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new elements and accessories."
+         name='Element Festival', time="8pm", date="01/03/2026", description="Festival of elements starts in January and and ends in March.."
     )
     event8 = Event(
-         name='Magic Festival', time="8pm", date="01/03/2026", description="Festival of magic starts in January and and ends in March. This festival is a celebration of the magic and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new magic and accessories."
+         name='Magic Festival', time="8pm", date="01/03/2026", description="Festival of magic starts in January and and ends in M."
     )
     event9 = Event(
-         name='Wizardry Festival', time="8pm", date="01/03/2026", description="Festival of wizardry starts in January and and ends in March. This festival is a celebration of the wizardry and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new wizardry and accessories."
+         name='Wizardry Festival', time="8pm", date="01/03/2026", description="Festival of wizardry starts in January and and ends in March. ."
     )
     event10 = Event(
-         name='Sorcery Festival', time="8pm", date="01/03/2026", description="Festival of sorcery starts in January and and ends in March. This festival is a celebration of the sorcery and its power. It is a time for wizards to come together and share their knowledge and skills with one another. The festival includes competitions, demonstrations, and workshops, as well as opportunities to purchase new sorcery and accessories."
+         name='Sorcery Festival', time="8pm", date="01/03/2026", description="Festival of sorcery starts in January and and ends in March."
     )
 
 
