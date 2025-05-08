501d19c68da8f18923a91434f1b148e9a323cc5d
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-05-07 21:56:54 -0400

    render please

--- app/seeds/monsters.py
+++ app/seeds/monsters.py
@@ -4,25 +4,25 @@ from sqlalchemy.sql import text
 
 def seed_monsters():
     monster1 = Monster(
-        name="GREEN DRAGON", element='TIME', description="This Monster has been on the loose for the longest time now.", url="none",health=100000, gold_drop=320000)
+        name="GREEN DRAGON", element='TIME', description="This Monster has been on the loose for the longest time now.", url="none",health=100000.00, gold_drop=320000.99)
     monster2 = Monster(
-        name="RED DRAGON", element='LIGHTNING', description="This Monster has been on the loose for the longest time now.", url="none",health=100000, gold_drop=320000)
+        name="RED DRAGON", element='LIGHTNING', description="This Monster has been on the loose for the longest time now.", url="none",health=.00, gold_drop=320000.99)
     monster3 = Monster(
-        name="BLUE DRAGON", element='FIRE', description="This Monster has been on the loose for the longest time now.", url="none", health=100000, gold_drop=320000)
+        name="BLUE DRAGON", element='FIRE', description="This Monster has been on the loose for the longest time now.", url="none", health=.00, gold_drop=320000.99)
     monster4 = Monster(
-        name="BLACK DRAGON", element='EARTH', description="This Monster has been on the loose for the longest time now.", url="htnone",health=100000, gold_drop=320000)
+        name="BLACK DRAGON", element='EARTH', description="This Monster has been on the loose for the longest time now.", url="htnone",health=.00, gold_drop=320000.99)
     monster5 = Monster(
-        name="WHITE DRAGON", element='WIND', description="This Monster has been on the loose for the longest time now.", url="htnone",health=100000, gold_drop=320000)
+        name="WHITE DRAGON", element='WIND', description="This Monster has been on the loose for the longest time now.", url="htnone",health=.00, gold_drop=320000.99)
     monster6 = Monster(
-        name="YELLOW DRAGON", element='WATER', description="This Monster has been on the loose for the longest time now.", url="htnone",health=100000, gold_drop=320000)
+        name="YELLOW DRAGON", element='WATER', description="This Monster has been on the loose for the longest time now.", url="htnone",health=.00, gold_drop=320000.99)
     monster7 = Monster(
-        name="BROWN DRAGON", element='EARTH', description="This Monster has been on the loose for the longest time now.", url="htnone",health=100000, gold_drop=320000)
+        name="BROWN DRAGON", element='EARTH', description="This Monster has been on the loose for the longest time now.", url="htnone",health=.00, gold_drop=320000.99)
     monster8 = Monster(
-        name="PURPLE DRAGON", element='FIRE', description="This Monster has been on the loose for the longest time now.", url="htnone",health=100000, gold_drop=320000)
+        name="PURPLE DRAGON", element='FIRE', description="This Monster has been on the loose for the longest time now.", url="htnone",health=.00, gold_drop=320000.99)
     monster9 = Monster(
-        name="PINK DRAGON", element='LIGHTNING', description="This Monster has been on the loose for the longest time now.", url="htnone",health=100000, gold_drop=320000)
+        name="PINK DRAGON", element='LIGHTNING', description="This Monster has been on the loose for the longest time now.", url="htnone",health=.00, gold_drop=320000.99)
     monster10 = Monster(
-        name="ORANGE DRAGON", element='WATER', description="This Monster has been on the loose for the longest time now.", url="htnone",health=100000, gold_drop=320000)
+        name="ORANGE DRAGON", element='WATER', description="This Monster has been on the loose for the longest time now.", url="htnone",health=.00, gold_drop=320000.99)
 
     db.session.add(monster1)
     db.session.add(monster2)
